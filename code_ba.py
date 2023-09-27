d5a81b51ca3976d4e780c21a30a402b8e546a185
Joocheol <joocheol@yonsei.ac.kr>
2023-09-27 15:33:22 +0900

    ok

---
+++ code_ba.py
@@ -0,0 +1,12 @@
+class BaseAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+
+
+
+sample_csa = CausalSelfAttention(num_heads=2, key_dim=512)
+    # continued                                                                .
