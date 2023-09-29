2eb8e78c8053dccb647a3e9983537c5f1bb040c7
Joocheol <joocheol@yonsei.ac.kr>
2023-09-29 15:07:59 +0900

    ok

---
+++ 02.py
@@ -0,0 +1,6 @@
+from manim import *
+from manim_voiceover import VoiceoverScene
+from manim_voiceover.services.azure import AzureService
+
+class Explainer(VoiceoverScene):
+    def construct(self):
