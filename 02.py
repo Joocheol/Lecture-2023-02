049b371df6f292a1ee66e769df8f6a18a1e415b1
Joocheol <joocheol@yonsei.ac.kr>
2023-09-29 15:57:32 +0900

    ok

--- 02.py
+++ 02.py
@@ -2,5 +2,22 @@ from manim import *
 from manim_voiceover import VoiceoverScene
 from manim_voiceover.services.azure import AzureService
 
+from transformer_functions import *
+
+# Multi head attention
+
 class Explainer(VoiceoverScene):
     def construct(self):
+
+        box = setup_text_box()
+        
+        self.play(box[0].rotate(-PI/2).animate.to_edge(LEFT))
+
+        rect_1 = Rectangle(width=3, height=0.2).set_fill(YELLOW, opacity=0.5)
+        rect_2 = Rectangle(width=3, height=0.2).set_fill(WHITE, opacity=0.5)
+        rect_3 = Rectangle(width=3, height=0.2).set_fill(BLUE, opacity=0.5)
+
+        rect = VGroup(rect_1, rect_2, rect_3).arrange(DOWN, buff=0.1)
+        self.play(rect.animate.next_to(box[0][0], RIGHT))
+        
+        self.wait()
--- transformer_functions.py
+++ transformer_functions.py
@@ -1,6 +1,14 @@
 from manim import *
 from constants import *
 
+small_buff = 0.3
+block_buff = 1.0
+btn = 1.5
+
+width = 3.0
+height = 0.6
+
+opacity = 0.3
 
 
 def setup():
