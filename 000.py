355e50d0203ed0d498ea97e52c354ceb4eff38b4
Joocheol <joocheol@yonsei.ac.kr>
2023-10-02 19:02:12 +0900

    ok

---
+++ 000.py
@@ -0,0 +1,40 @@
+from manim import *
+from manim_voiceover import VoiceoverScene
+from manim_voiceover.services.azure import AzureService
+
+from my_transformer import *
+
+import numpy as np
+
+class Explainer(VoiceoverScene):
+    def speech_setup(self, file):
+        self.set_speech_service(
+            AzureService(
+                voice="en-US-JennyMultilingualNeural",
+                style="newscast-casual",
+            )
+        )
+
+        with open(file, "r") as f:
+            n = f.read()
+        n = n.splitlines()
+
+        text = ["""<lang xml:lang="ko-KR">""" + "줄 번호를 확인하기 바랍니다." +"""</lang>"""]
+        for i in n:
+            text.append("""<lang xml:lang="ko-KR">""" + i +"""</lang>""")
+
+        return text[:]
+    
+    def construct(self):
+        self.text = self.speech_setup("000.txt")
+
+        with self.voiceover(self.text[1]) as tracker:
+            pass
+
+        tracker = self.add_voiceover_text(self.text[2])
+        self.wait()
+        self.wait(tracker.get_remaining_duration(buff=0.5))
+
+
+        
+        self.wait()
